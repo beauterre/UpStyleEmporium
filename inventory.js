1c04fa0c1f118c111fcee3c1f49784745138e62d
Belle Beauterre-Babbillard <26629952+beauterre@users.noreply.github.com>
2025-10-16 19:03:02 +0200

    reuse showSection

--- inventory.js
+++ inventory.js
@@ -13,8 +13,6 @@ document.querySelectorAll('.filter-buttons .filter').forEach(function(btn){
 
 // Hookup the "Edit Filters" button
 document.getElementById('edit_filters').addEventListener('click', function() {
-  // Hide any other hidden sections
-  VASman.Hidden.hideAll();
   // Show the filter CRUD hidden section
-  VASman.Hidden.show('filter-crud');
+  VASman.showSectionInternal('filter-crud');
 });
